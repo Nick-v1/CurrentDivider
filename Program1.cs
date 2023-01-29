e77a457b33ce41db833d45b6360d6f88e9b06275
Nick-v1 <nikos.voulgaridis1@gmail.com>
2023-01-30 01:41:11 +0200

    Add project files.

---
+++ Program1.cs
@@ -0,0 +1,41 @@
+﻿namespace CurrentDivider
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Current Divider Circuits.
+        /// Inputs: 
+        ///     1) Current: (Ampere),
+        ///     2) The resistor value we want to find the current on,
+        ///     3) The resistor that is connected in parallel.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Current (Ampere): ");
+                var reuma = Double.Parse(Console.ReadLine().ToString());
+
+                Console.Write("Wanted Resistor(Ohm): ");
+                var zitoumeniAntistasi = Double.Parse(Console.ReadLine().ToString());
+
+                Console.Write("Parallel Resistor(Ohm): ");
+                var apentantiAntistasi = Double.Parse(Console.ReadLine().ToString());
+
+
+
+                double result = reuma * apentantiAntistasi / (apentantiAntistasi + zitoumeniAntistasi);
+
+                Console.WriteLine($"Current in wanted resistor is: {result} A");
+
+                Console.Write("Continue?: ");
+                var cont = Console.ReadKey().KeyChar.ToString();
+                Console.WriteLine("\n");
+
+                if (cont.Equals("n"))
+                    Environment.Exit(0);
+            }
+        }
+    }
+}
